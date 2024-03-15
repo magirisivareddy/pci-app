1bfb6ba4e0c25a374f923d18460b956be19ddc7e
Siva Prasad Reddy Magiri <smagiri@evoketechnologies.com>
2024-03-15 14:58:08 +0530

    remvoing unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
 
-import { fetchInspectors, fetchVenue } from "@/actions/api";
 import CustomBreadcrumbs from "@/components/common/breadcrumb/Breadcrumb";
 import { Suspense } from "react";
 import Loading from "./loading";
 import Inspections from "@/components/inspections/Inspections";
-export default async function Home() {
-  const venues = await fetchVenue();
-  const inspectors = await fetchInspectors();
+export default function Home() {
   return (
     <main >
       {/* <CustomBreadcrumbs /> */}
       <Suspense fallback={<Loading />}>
-        <Inspections venueDropdown={venues} inspectorsDropdown={inspectors}/>
+        <Inspections/>
       </Suspense>
 
     </main>
